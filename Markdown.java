56013bbef53f5b78cab36fda13bb4f7f0efeadc1
as000fm <61089329+as000fm@users.noreply.github.com>
2024-06-07 06:41:10 -0400

    Update Markdown.java

--- Markdown.java
+++ Markdown.java
@@ -38,3 +38,44 @@
 
 		return htmlText;
 	}
+
+	/**
+	 * Traitement des méta-caractères d'une expression regulière
+	 * @param value Le texte de l'expression regulière à traiter
+	 * @return le texte de l'expression regulière traité
+	 */
+	final public static String escapeRegExpMetaChars(String value) {
+		if (isEmpty(value)) {
+			return value;
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		for (char c : value.toCharArray()) {
+			switch (c) {
+				case '\\':
+				case '"':
+				case '^':
+				case '$':
+				case '.':
+				case '|':
+				case '?':
+				case '*':
+				case '+':
+				case '(':
+				case ')':
+				case '[':
+				case ']':
+				case '{':
+				case '}':
+					sb.append('\\');
+					sb.append(c);
+					break;
+				default:
+					sb.append(c);
+					break;
+			}
+		}
+
+		return sb.toString();
+	}

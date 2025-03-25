40fd46fbba221359976d372b03c1b6f4a388e917
as000fm <61089329+as000fm@users.noreply.github.com>
2025-03-25 10:29:40 -0400

    Update asList.java

--- asList.java
+++ asList.java
@@ -19,3 +19,26 @@
 		return list;
 	}
 
+	/**
+	 * Extrait les données de la vue sous forme de liste
+	 * @param max Le nombre maximum d'enregistrements à extraire
+	 * @return les données de la vue sous forme de liste
+	 * @throws DataSetException en cas d'erreur...
+	 */
+	public List<T> asList(int max) throws DataSetException {
+		if (!isActive()) {
+			open();
+		}
+
+		List<T> list = new ArrayList<T>();
+
+		while ((max > 0) && !isEof()) {
+			max--;
+
+			list.add(getRowData());
+
+			next();
+		}
+
+		return list;
+	}
